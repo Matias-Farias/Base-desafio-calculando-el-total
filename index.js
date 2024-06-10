88cc834e88066f110244f2b829717f76f74bb880
Matias-Farias <166570790+Matias-Farias@users.noreply.github.com>
2024-06-10 16:32:34 -0400

    commit first

---
+++ index.js
@@ -0,0 +1,7 @@
+precio = 400000
+let valortotal = 0;
+let cantidad = 0;
+
+cantidadSpan = document.querySelector('.cantidad')
+totalSpan = document.querySelector('.valor-total')
+precioSpan = document.querySelector('.precio-inicial');
